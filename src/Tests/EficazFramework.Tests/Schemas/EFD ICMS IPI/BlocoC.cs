dede84edc87072a953350b751bb434286eb3baa7
Henrique Clausing <hclausing@gmail.com>
2022-07-20 23:34:57 -0300

    Implementação de patterns em testes. Fase 1

--- src/Tests/EficazFramework.Tests/Schemas/EFD ICMS IPI/BlocoC.cs
+++ src/Tests/EficazFramework.Tests/Schemas/EFD ICMS IPI/BlocoC.cs
@@ -12,27 +12,16 @@ public class BlocoC : Tests.BaseTest
         reg.Codigo.Should().Be("C500");
     }
 
+    
     [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|5555||||||", "015")]
     [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|5555|||2|||||||122021|||", "016", true)]
     public void RegistroC500_Construtor(string linha, string versao = "016", bool indicadorContribIcms = false)
     {
         var reg = new EficazFramework.SPED.Schemas.EFD_ICMS_IPI.RegistroC500(linha, versao);
-        reg.Codigo.Should().Be("C500");
-        reg.Versao.Should().Be(versao);
-        reg.Operacao.Should().Be(IndicadorOperacao.Entrada);
-        reg.Emissao.Should().Be(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.IndicadorEmitente.Terceiros);
-        reg.CodigoParticipante.Should().Be("123");
-        reg.EspecieDocumento.Should().Be("06");
-        reg.SituacaoDocumento.Should().Be(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.SituacaoDocumento.Regular);
-        reg.Numero.Should().Be(12345678);
-        reg.Serie.Should().Be("U1");
-        reg.DataEmissao.Should().Be(new System.DateTime(2021, 6, 18));
-        reg.ValorTotalDocumento.Should().Be(539.8d);
-        
-        if (indicadorContribIcms)
-            reg.DestinatarioIndicador.Should().Be(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.IndicadorDestinatario.ContribIsentoICMS);
+        RegistroC500_InternalRead(reg, versao, indicadorContribIcms);
     }
 
+    
     [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|", "002")]
     [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|", "003")]
     [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|5555||||||", "014")]
@@ -72,12 +61,50 @@ public class BlocoC : Tests.BaseTest
         reg.ToString().Should().Be(result);
     }
 
-    
-    [TestCase("|C500|0|1|2378|06|00|||01|52005264|03062020|01072020|473,38|0|726|0|0|0|0|0|0|0||7,61|35,06|3|||||1|3556701|206|", "014")]
-    public void RegistroC500_Leitura(string linha, string versao = "016")
+
+    [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|", "003")]
+    [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|5555||||||", "014")]
+    [TestCase("|C500|0|1|123|06|00|U1||01|12345678|18062021|18062021|539,8||530|||9,8|539,8|100||||6|42|3|01|5555|||2|||||||122021|||", "016", true)]
+    public void RegistroC500_Leitura(string linha, string versao = "016", bool indicadorContribIcms = false)
     {
-        var reg = new EficazFramework.SPED.Schemas.EFD_ICMS_IPI.RegistroC500(linha, versao);
+        var reg = new EficazFramework.SPED.Schemas.EFD_ICMS_IPI.RegistroC500("", versao);
+        reg.CodigoParticipante.Should().Be(null);
+        reg.ValorTotalDocumento.HasValue.Should().Be(false);
+
         reg.LeParametros(linha.Split('|'));
+        RegistroC500_InternalRead(reg, versao, indicadorContribIcms);
     }
 
+    
+    private void RegistroC500_InternalRead(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.RegistroC500 reg, string versao = "016", bool indicadorContribIcms = false)
+    {
+        reg.Codigo.Should().Be("C500");
+        reg.Versao.Should().Be(versao);
+        reg.Operacao.Should().Be(IndicadorOperacao.Entrada);
+        reg.Emissao.Should().Be(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.IndicadorEmitente.Terceiros);
+        reg.CodigoParticipante.Should().Be("123");
+        reg.EspecieDocumento.Should().Be("06");
+        reg.SituacaoDocumento.Should().Be(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.SituacaoDocumento.Regular);
+        reg.Numero.Should().Be(12345678);
+        reg.Serie.Should().Be("U1");
+        reg.DataEmissao.Should().Be(new System.DateTime(2021, 6, 18));
+        reg.ValorTotalDocumento.Should().Be(539.8d);
+
+        if (int.Parse(versao) >= 14)
+            reg.ChaveDocE.Should().Be("5555");
+        else
+            reg.ChaveDocE.Should().BeNull();
+        
+
+        if (int.Parse(versao) >= 16)
+        {
+            reg.CompetenciaDocumentoReferenciado.Value.Month.Should().Be(12);
+            reg.CompetenciaDocumentoReferenciado.Value.Year.Should().Be(2021);
+        }
+
+
+        if (indicadorContribIcms)
+            reg.DestinatarioIndicador.Should().Be(EficazFramework.SPED.Schemas.EFD_ICMS_IPI.IndicadorDestinatario.ContribIsentoICMS);
+
+    }
 }
