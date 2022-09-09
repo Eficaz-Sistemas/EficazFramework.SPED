222517d8223f29059972d28a5dd64ca042d91a40
Henrique Clausing <hclausing@gmail.com>
2022-09-09 14:11:29 -0300

    Desmembramento dos registros da EFD-Reinf em arquivos; Adição de global usings.

--- src/Core/EficazFramework.SPED/using.cs
+++ src/Core/EficazFramework.SPED/using.cs
@@ -1,6 +1,9 @@
 ﻿global using System;
 global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
-global using System.Diagnostics.CodeAnalysis;
+global using System.Xml.Serialization;
