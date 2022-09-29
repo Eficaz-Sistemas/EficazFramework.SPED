e1bc2900e27c3eedd37b52b670f243652b78188c
OtavioVB <otaviovb.developer@gmail.com>
2022-09-29 18:40:29 -0300

    feat(functions): `create general to writeline`

---
+++ src/Core/EficazFramework.SPED/Utilities/Registros/Functions.cs
@@ -0,0 +1,22 @@
+﻿namespace EficazFramework.SPED.Utilities.Registros;
+    
+public static class Functions
+{
+    internal static string ConvertStringArrayInDefaultSchema(string[] data)
+    {
+        var schemaDefault = new StringBuilder();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (i == 0)
+            {
+                schemaDefault.Append(data[i]);
+                continue;
+            }
+
+            schemaDefault.Append("|" + data[i]);
+        }
+
+        return schemaDefault.ToString();
+    }
+}
