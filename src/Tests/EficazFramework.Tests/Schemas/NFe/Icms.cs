7937026da3db13632c9f317b7537ae74703bcc1c
Henrique Clausing <hclausing@gmail.com>
2024-03-22 14:38:46 -0300

    NFe: Refactoring de classes de tributação, alterando a property "DemaisTributos" para "IcmsIpiIssqnIi"

--- src/Tests/EficazFramework.Tests/Schemas/NFe/Icms.cs
+++ src/Tests/EficazFramework.Tests/Schemas/NFe/Icms.cs
@@ -11,11 +11,11 @@ public class Icms : BaseXmlTest<ProcessoNFe>
     {
         ProcessoNFe instance = await ReadAsync(Resources.Schemas.XML.NFCeNt2023_001);
         instance.Should().NotBeNull();
-        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.DemaisImpostos[0]).Tributacao.Origem.Should().Be(OrigemMercadoria.Nacional);
-        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.DemaisImpostos[0]).Tributacao.CST.Should().Be(CST_ICMS.CST_61);
-        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.DemaisImpostos[0]).Tributacao.qBCMonoRet.Should().Be(16.761d);
-        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.DemaisImpostos[0]).Tributacao.adRemICMSRet.Should().Be(1.2200d);
-        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.DemaisImpostos[0]).Tributacao.vICMSMonoRet.Should().Be(20.44d);
+        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.IcmsIpiIssqnIi[0]).Tributacao.Origem.Should().Be(OrigemMercadoria.Nacional);
+        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.IcmsIpiIssqnIi[0]).Tributacao.CST.Should().Be(CST_ICMS.CST_61);
+        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.IcmsIpiIssqnIi[0]).Tributacao.qBCMonoRet.Should().Be(16.761d);
+        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.IcmsIpiIssqnIi[0]).Tributacao.adRemICMSRet.Should().Be(1.2200d);
+        ((EficazFramework.SPED.Schemas.NFe.DetalhamentoICMS)instance.NFe.InformacoesNFe.Items[0].Imposto.IcmsIpiIssqnIi[0]).Tributacao.vICMSMonoRet.Should().Be(20.44d);
     }
 
 }
