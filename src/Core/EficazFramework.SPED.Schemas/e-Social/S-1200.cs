cc279fcaa73dfb4d8fd8800a03e8fe1b4b03a35d
Henrique Clausing <hclausing@gmail.com>
2025-04-16 09:38:47 -0300

    Progresso de mapeamento do evento S-1200, relativo a multiplos vínculos

--- src/Core/EficazFramework.SPED.Schemas/e-Social/S-1200.cs
+++ src/Core/EficazFramework.SPED.Schemas/e-Social/S-1200.cs
@@ -38,6 +38,7 @@ public class S1200 : Evento
     public override void GeraEventoID()
         => evtRemun.Id = string.Format("ID{0}{1}{2}", (int)(evtRemun?.ideEmpregador?.tpInsc ?? PersonalidadeJuridica.CNPJ), evtRemun?.ideEmpregador?.NumeroInscricaoTag() ?? "00000000000000", eSocialTimeStampUtils.GetTimeStampIDForEvent());
 
+
     // IXmlSignableDocument Members
     /// <exclude/>
     public override string TagToSign => Evento.root;
@@ -109,4 +110,93 @@ public class EvtRemun : ESocialBindableObject
     }
 }
 
+public class InfoMultiplosVinculos : ESocialBindableObject
+{
+    private int indMV = 3;
+    private List<RemuneracaoOutrasEmpresas> remunOutrEmpr;
 
+    /// <summary>
+    /// <b>Valores válidos:</b><br/><br/>
+    /// 1. O declarante aplica a(s) alíquota(s) de desconto do segurado sobre a remuneração por ele 
+    /// informada (o percentual da(s) alíquota(s) será(ão) obtido(s) considerando a remuneração total 
+    /// do trabalhador)<br/><br/>
+    /// 2. O declarante aplica a(s) alíquota(s) de desconto do segurado sobre a diferença entre o 
+    /// limite máximo do salário de contribuição e a remuneração de outra(s) empresa(s) para as quais 
+    /// o trabalhador informou que houve o desconto<br/><br/>
+    /// 3 - O declarante não realiza desconto do segurado, uma vez que houve desconto sobre o limite 
+    /// máximo de salário de contribuição em outra(s) empresa(s)
+    /// </summary>
+    [XmlElement(ElementName = "indMV")]
+    public int IndMV
+    {
+        get => indMV;
+        set
+        {
+            indMV = value;
+            RaisePropertyChanged(nameof(IndMV));
+        }
+    }
+
+    [XmlElement(ElementName = "remunOutrEmpr")]
+    public List<RemuneracaoOutrasEmpresas> RemunOutrEmpr
+    {
+        get => remunOutrEmpr;
+        set
+        {
+            remunOutrEmpr = value;
+            RaisePropertyChanged(nameof(RemunOutrEmpr));
+        }
+    }
+}
+
+public class RemuneracaoOutrasEmpresas : ESocialBindableObject
+{
+    private PersonalidadeJuridica tpInsc;
+    private string nrInsc;
+    private string codCateg;
+    private decimal vlrRemunOE;
+
+    [XmlElement(ElementName = "tpInsc")]
+    public PersonalidadeJuridica TpInsc
+    {
+        get => tpInsc;
+        set
+        {
+            tpInsc = value;
+            RaisePropertyChanged(nameof(TpInsc));
+        }
+    }
+
+    [XmlElement(ElementName = "nrInsc")]
+    public string NrInsc
+    {
+        get => nrInsc;
+        set
+        {
+            nrInsc = value;
+            RaisePropertyChanged(nameof(NrInsc));
+        }
+    }
+
+    [XmlElement(ElementName = "codCateg")]
+    public string CodCateg
+    {
+        get => codCateg;
+        set
+        {
+            codCateg = value;
+            RaisePropertyChanged(nameof(CodCateg));
+        }
+    }
+
+    [XmlElement(ElementName = "vlrRemunOE")]
+    public decimal VlrRemunOE
+    {
+        get => vlrRemunOE;
+        set
+        {
+            vlrRemunOE = value;
+            RaisePropertyChanged(nameof(VlrRemunOE));
+        }
+    }
+}
