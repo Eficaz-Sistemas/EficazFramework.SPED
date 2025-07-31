ba1471b2e2e8c115e9cc685f3b04a45b0b25c079
Henrique Clausing <hclausing@gmail.com>
2025-07-31 12:20:03 -0300

    code cleanup for DFeBase

--- src/Core/EficazFramework.SPED.Schemas/DFeBase/DFeBase.cs
+++ src/Core/EficazFramework.SPED.Schemas/DFeBase/DFeBase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
 // REFERE-SE AOS TIPOS BÁSICOS COMUNS PARA TODOS OS SCHEMAS DO SPED
 // RELATIVOS AOS TRIBUTOS DA REFORMA TRIBUTÁRIA: IBS, CBS E IS
